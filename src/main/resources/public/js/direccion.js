538d4e4d03d448ff0af05ecbdbdf8a42fdc78bab
Fernandodaniel <Fernandodaniel@Velazquez>
2017-06-13 21:26:22 -0500

    EXCEPCIONES

--- src/main/resources/public/js/direccion.js
+++ src/main/resources/public/js/direccion.js
@@ -4,4 +4,24 @@
  * and open the template in the editor.
  */
 
-
+//Vamos hacer el request a nuestro servicio usando ajax con jquery
+$("#guardarDireccion").click(function(){
+    //Aplicamos el metodo post usando la uri del servicio
+   var numero=$("#numero").val();
+   var calle=$("#calle").val();
+   var cp=$("#cp").val();
+   var municipio=$("#municipio").val();
+    $.post("direccion/"+numero+ '/' +calle+'/'+cp+'/'+municipio,function(json){
+        console.log(json.id);
+    });
+    }
+    );
+    
+$("#buscarPorId").click(function(){
+    //Aplicamos el metodo post usando la uri del servicio
+   var id=$("#id").val();
+   $.get("direccion/"+id,function(json){
+        console.log(json.municipio);
+    });
+}
+);
